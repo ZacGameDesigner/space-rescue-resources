97e8ec853567a5dc380c1d8a1ed3f59a1aa7b2bf
ZacGameDesigner <27evansz@mbbc.qld.edu.au>
2024-05-07 14:56:41 +1000

    added Astronaut

---
+++ Objects/Astronaut.py
@@ -0,0 +1,40 @@
+from GameFrame import RoomObject
+from GameFrame.Level import Level
+
+class Astronaut(RoomObject):
+    
+    def __init__(self,room,x,y):
+        """
+        Initialise the astronaut instance
+        """
+        # include attirbutes and method from RoomObject
+        RoomObject.__init__(self,room,x,y)
+        
+        # set image
+        image = self.load_image("Astronaut.png")
+        self.set_image(image,50,49)
+        
+        # set travel direction
+        self.set_direction(180, 5)
+        
+        # handle events
+        self.register_collision_object("Ship")
+        
+    def step(self):
+        """
+        Determines what happend to the astronaut on each tick of the game clock
+        """
+        self.outside_of_room()
+    
+    def handle_collision(self, other, other_type):
+        #handles collision for Astronaut object
+        if other_type == "Ship":
+            self.room.delete_object(self)
+    
+    def outside_of_room(self):
+        # removes astronauts when outside of room
+        
+        if self.x + self.width < 0:
+            self.room.delete_object(self)
+        
+        
