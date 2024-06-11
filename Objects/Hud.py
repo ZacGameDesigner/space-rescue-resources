af510a2b9bf83fad5c7590dd9f1a73cfaa40d2c6
ZacGameDesigner <27evansz@mbbc.qld.edu.au>
2024-06-11 14:51:49 +1000

    Added lives

--- Objects/Hud.py
+++ Objects/Hud.py
@@ -1,5 +1,4 @@
-from GameFrame import TextObject, Globals
-from GameFrame.Level import Level
+from GameFrame import TextObject, Globals, RoomObject
 
 class Score(TextObject):
     # class for displaying score
@@ -21,3 +20,24 @@ class Score(TextObject):
         Globals.SCORE += change
         self.text = str(Globals.SCORE)
         self.update_text()
+
+class Lives(RoomObject):
+    
+    def __init__(self, room, x: int, y: int):
+        
+        # Intialises the lives object
+        
+        RoomObject.__init__(self, room, x, y)
+        
+        #set image
+        self.lives_icon = []
+        
+        #load the various lives images into a live list
+        for index in range(6):
+            self.lives_icon.append(self.load_image(f"Lives_frames/Lives_{index}.png"))
+        self.update_image()
+    
+    def update_image(self):
+        # updates the number of lives on the UI
+        
+        self.set_image(self.lives_icon[Globals.LIVES], 125, 23)
